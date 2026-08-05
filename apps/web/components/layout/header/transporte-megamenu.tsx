58e09086daf586b4920bcdca0911d65c6b21d9fa
acrovtech <acrovtech@gmail.com>
2026-08-05 15:57:14 -0500

    fix(transporte-megamenu): remove Servicio Privado Salkantay Trek item

--- apps/web/components/layout/header/transporte-megamenu.tsx
+++ apps/web/components/layout/header/transporte-megamenu.tsx
@@ -17,14 +17,7 @@ export function TransporteMegamenu() {
                 Cusco a Soraypampa (Ida y Vuelta)
               </Link>
             </li>
-            <li className="my-2 transition-all duration-300 hover:translate-x-2">
-              <Link 
-                href="/transporte" 
-                className="block py-2 pl-4 border-l-4 border-white/70 border-b border-white/20 rounded-md transition-all duration-300 hover:bg-white/10 hover:shadow-[0_0_15px_rgba(255,255,255,0.1)] text-white/80 hover:text-white text-sm"
-              >
-                Servicio Privado Salkantay Trek
-              </Link>
-            </li>
+
           </ul>
         </div>
 
